3850af14e862d31590a013fc38ce6ece120f1aa5
Max K <maxus@list.ru>
2020-10-14 11:32:59 +0500

    Update Server_Raspberry_TALLY.py

--- Server_Raspberry_TALLY.py
+++ Server_Raspberry_TALLY.py
@@ -3,11 +3,13 @@
 from getmac import get_mac_address
 import socket, struct
 from time import sleep
-import RPi.GPIO as GPIO
+import OPi.GPIO as GPIO
 import subprocess
+import urllib2
 #### 50.100 - 50.110
-
-sleep(30)
+GPIO.setboard(GPIO.ZERO)
+GPIO.setmode(GPIO.BOARD)
+#sleep(30)
 
 UDP_IP = "192.168.50.100"
 UDP_PORT = 4213
@@ -15,13 +17,23 @@ UDP_PORT = 4213
 MESSAGE_A = "A"
 BUF_IP = [1,2,3,4,5,6,7,8,9,10]
 
-PIN_PGM = [26, 19, 13 , 6, 5]
+#PIN_PGM = [26, 24, 22 , 16, 18]
+PIN_PGM = [26, 24, 22 , 10, 12]
 PIN_PRE = []
 
 ip2int = lambda ipstr: struct.unpack('!I', socket.inet_aton(UDP_IP))[0]
 int2ip = lambda n: socket.inet_ntoa(struct.pack('!I', n))
 
 INT_IP = ip2int(UDP_IP)
+
+def startVMIX():
+  try:
+#  e = urlopen('http://172.26.2.165:8088/api/?Function=Prerview&Input=1')
+     e = urllib2.urlopen('http://172.26.2.165:8088/api/?Function=Cut')
+#  e = urlopen('http://172.26.2.165:8088/api/?Function=Play&Input=1')
+  except urllib2.URLError as ee:
+     print "urllib.error.HTTPError"
+
 
 def restartWIFI():
   proc = subprocess.Popen('sudo ifdown wlan0', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -45,11 +57,11 @@ def setupPi():
 def scanIP():
   global MESSAGE_A, UDP_IP, UDP_PORT, INT_IP, BUF_IP
   BUF_IP = []
-  for inc in range(11):
+  for inc in range(16):
      IIP = get_mac_address(ip=int2ip(INT_IP+inc))
      if (IIP[0] <> '0'):
          BUF_IP.append(inc)
-  print BUF_IP
+  #print BUF_IP
   return True
 
 
@@ -61,14 +73,14 @@ def ESPsend(mes, sock):
 #      sock.setblocking(0)
 #      print socket.gethostname()
       sock.sendto(mes, (int2ip(INT_IP+inc), UDP_PORT+100+inc))
-      print UDP_PORT, inc, UDP_PORT+100+inc
+#      print UDP_PORT, inc, UDP_PORT+100+inc
       sock.close()
   return True
 
 
 def sendUDPall(mes):
   global MESSAGE_A, UDP_IP, UDP_PORT, INT_IP
-  for inc in range(11):
+  for inc in range(16):
       sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
       sock.sendto(mes, (int2ip(INT_IP+inc), UDP_PORT))
       sleep(0.2)
@@ -84,6 +96,7 @@ def readGPIO():
       B = GPIO.input(pin)
       STATUS = STATUS | B
       STATUS = STATUS << 1
+  ##  print "read GPIO char str int" , chr(STATUS+50) ,str(STATUS), STATUS
   return STATUS
 
 ##**************************************************************************************
@@ -91,10 +104,10 @@ def main():
   setupPi()
   restartWIFI()
   global MESSAGE_A, UDP_IP, UDP_PORT, INT_IP
-  print MESSAGE_A
-  print UDP_IP
-  print UDP_PORT
-
+  #print MESSAGE_A
+  #print UDP_IP
+  #print UDP_PORT
+  flag = 0
 ##-------------------------------------
   count = 0
   mess = readGPIO()
@@ -102,12 +115,21 @@ def main():
   scanIP()
   while True:
     mes = readGPIO()
+##++++++++++++[REMOTE VMIX]
+    if (mes == 62):
+        if (flag == 0):
+                startVMIX()
+                flag = 1
+    else:
+                flag = 0
+##+++++++++++++++++++++++++
+
     if (mess <> mes):
         #sendUDPall( chr(mes))
         ESPsend(chr(mes), sockets)
         mess = mes
         count = 0
-        print "mes", mes
+#        print "mes", mes
         sleep(0.2)
     else:
         sleep(0.2)
@@ -117,6 +139,7 @@ def main():
            scanIP()
 
   return True
+
 
 
 ##************************************************************************************
